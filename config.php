48028530fad73c0f29fc98cf426b325d847566b1
Zero Two <71703768+Avenger41@users.noreply.github.com>
2020-10-20 13:34:48 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,3 +1,3 @@
 <?php 
-$cn=mysqli_connect("ftp.gear.host","root","lux4cd9NArDq14k6fuELac7vbXtuufBZ3ZQpfAxRskBaCiXF6Wau2h9fTFAZ","cloud-computing\$cloud-computing");
+$cn=mysqli_connect("ftp.gear.host","lux4cd9NArDq14k6fuELac7vbXtuufBZ3ZQpfAxRskBaCiXF6Wau2h9fTFAZ","cloud-computing\$cloud-computing");
 ?>
